afa80229ea11f24f80c45be5d788773d1c1eea74
amitg01 <amitg.ghy@gmail.com>
2021-07-31 11:56:56 +0530

    Added test coverage reporting

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
+def enable_test_coverage
+  require "simplecov"
+  SimpleCov.start do
+    add_filter "/test/"
+    add_group "Models", "app/models"
+    add_group "Mailers", "app/mailers"
+    add_group "Controllers", "app/controllers"
+    add_group "Uploaders", "app/uploaders"
+    add_group "Helpers", "app/helpers"
+    add_group "Workers", "app/workers"
+    add_group "Services", "app/services"
+  end
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+enable_test_coverage if ENV["COVERAGE"]
+
 class ActiveSupport::TestCase
   include ActionView::Helpers::TranslationHelper
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  parallelize(workers: :number_of_processors) unless ENV["COVERAGE"]
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
